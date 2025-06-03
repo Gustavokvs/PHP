3790cbc7e00f476a26e0ec933a625cf508ea266c
Gustavokvs <gustavokruger514@gmail.com>
2025-06-02 22:02:38 -0300

    Começando php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="titulo">
+        <h1>Exercícios</h1>
+    </div>
+    <div class="container">
+        <div class="links"><a href="./exer1/exer1.php">Exer1</a></div>
+        <div class="links"><a href="">Exer2</a></div>
+        <div class="links"><a href="">Exer3</a></div>
+        <div class="links"><a href="">Exer4</a></div>
+        <div class="links"><a href="">Exer5</a></div>
+        <div class="links"><a href="">Exer6</a></div>
+        <div class="links"><a href="">Exer7</a></div>
+        <div class="links"><a href="">Exer8</a></div>
+        <div class="links"><a href="">Exer9</a></div>
+        <div class="links"><a href="">Exer10</a></div>
+        <div class="links"><a href="">Exer11</a></div>
+    </div>
+</body>
+
+</html>
